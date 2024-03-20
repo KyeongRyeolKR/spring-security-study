3b98cee40199afa921810a5ade8d9a0aa49ad2e3
김경렬 <rlarudfuf802@naver.com>
2024-03-20 23:44:07 +0900

    feat: 커스텀 로그인 페이지

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +21,22 @@ public class SecurityConfig {
                                 .requestMatchers("/admin").hasRole("ADMIN") // /admin 경로는 ADMIN만 허용
                                 .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER") // /my/** 경로는 ADMIN, USER만 허용
                                 .anyRequest().authenticated() // 나머지 요청은 로그인한 사용자만 허용
+                );
+
+        http
+                // 커스텀 로그인 페이지 설정
+                .formLogin(
+                        (auth) -> auth
+                                .loginPage("/login")
+                                .loginProcessingUrl("/loginProc")
+                                .permitAll()
+                );
+
+        http
+                // 사이트 위조/변조 방지(CSRF) 설정
+                // 개발환경에서는 CSRF 토큰이 없어도 동작이 가능하도록 설정을 끔
+                .csrf(
+                        AbstractHttpConfigurer::disable
                 );
 
         return http.build();
