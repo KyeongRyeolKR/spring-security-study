55878b006c28ff5aa674b0885e3c4e6aafae11df
김경렬 <rlarudfuf802@naver.com>
2024-03-21 14:59:27 +0900

    feat: 세션 사용자 정보

--- src/main/java/com/example/security/controller/MainController.java
+++ src/main/java/com/example/security/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.example.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainP() {
+    public String mainP(Model model) {
+        // 인증 객체
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String id = authentication.getName(); // username
+        String role = authentication.getAuthorities().iterator().next().getAuthority(); // role
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
+
         return "main";
     }
 }
